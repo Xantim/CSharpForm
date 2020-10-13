9bbdbbbe1517652529e167668ba0d0f7d3c1c78e
Dennis Kim <ddkim4@gmail.com>
2020-10-12 20:23:06 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AS_DojoSurvey.Models;
+
+namespace AS_DojoSurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("user")]
+        public IActionResult User()
+        {
+            return View();
+        }
+
+        [HttpPost("PostMethod")]
+        public IActionResult PostMethod(User newUser)
+        {
+            // ViewBag.Name = newUser.Name;
+            // ViewBag.Email = newUser.Email;
+            // ViewBag.Password = newUser.Password;
+            // ViewBag.Location = newUser.Location;
+            // ViewBag.Language = newUser.Language;
+            // ViewBag.Comment = newUser.Comment;
+            return View("Success", newUser);
+        }
+
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AS_DojoSurvey.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
